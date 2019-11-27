6d7f19e95cb99c62380a08306391961748b74596
Aj <deajetrott@gmail.com>
2019-11-27 01:28:31 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,48 @@
+// Making default api route
+let apiRoot = "http://spotify-apii.herokuapp.com/songs/"
+
+// Making a onclick event for my spotify button that puts the value of event.target inside a var called name
+$("#spotify_button").on('click', (event) => {
+    let name = $(event.target).attr("name")
+    console.log(name)
+
+    // append apiRoot URL + the name that is found in the attribute "name" with a limit of 10
+    let url = apiRoot + name + "/5"
+
+    // Making a get request to the api and saves the data inside response
+    $.get(url, (response) => {
+        console.log(response)
+        console.log(response.length)
+
+        for (let index = 0; index < response.length; index++) {
+            let name = response[index].name
+            let popularity = response[index].popularity
+            let previewUrl = response[index].previewUrl
+            let urlImage = response[index].url
+            let artist = response[index].artist
+
+
+            let card = $("<div>").attr({
+                class: "card-body",
+                style: "display:flex;flex-wrap:wrap;width:fit-content; height:auto;padding:0px 30px 20px 30px"
+            }).html(`<img style="width:300px;height:auto;" src='${urlImage}'><h4 style='width:100%;margin-bottom:0px'>${name}</h4><p margin-top: 0px;><i class="em em-fire" aria-role="presentation" aria-label="FIRE"></i> ${popularity}</p><audio controls src="${previewUrl}" type="audio/mp3></audio">`)
+
+
+            $("div#inner_music").attr({
+                style: "border-top:3px red solid;padding-top:20px;display:flex;align-content:center;margin-top:225px;font-family:'circularBold';color:white;background: rgb(89,134,230); background-color: #2b4162;background-color: #000000;background-image: linear-gradient(147deg, #000000 0%, #04619f 74%);"
+            }).append(card)
+
+            $("#addHref").attr({
+                href: "#artist_title"
+            })
+            $("#artist_title").html(`Songs Featuring ${artist}`)
+
+
+        }
+
+
+    })
+})
+
+//Create a div for each track it finds
+//
